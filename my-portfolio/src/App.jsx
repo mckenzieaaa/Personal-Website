0d03314fe59a4ebd0db8f8b7322bfdca3b0e7f01
mckenzieaaa <McKenzie.ouyang@gmail.com>
2025-10-09 10:47:39 +0800

    Further reduce photo size to 80px height for smaller visual footprint

--- my-portfolio/src/App.jsx
+++ my-portfolio/src/App.jsx
@@ -926,7 +926,7 @@ function App() {
             {/* 个人照片 */}
             <div style={{
               width: '100%',
-              height: '100px',
+              height: '80px',
               background: 'rgba(255,255,255,0.05)',
               borderRadius: '16px',
               border: '1px solid rgba(255,255,255,0.1)',
